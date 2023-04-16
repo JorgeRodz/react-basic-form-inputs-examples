d43a0a530a6a7731f0d4cb4d3166e85dae34d351
Jorge Rodriguez <jarf4321@gmail.com>
2023-04-16 12:48:25 -0600

    Add basic "uncontrolled component" form

---
+++ src/FormUncontrolledComponent.js
@@ -0,0 +1,21 @@
+// form uncontrolled component - basic example
+function FormUncontrolledComponent() {
+  const submit = (e) => {
+    e.preventDefault()
+    const data = Array.from(new FormData(e.target)) // Here we get the data from the form, and store as an Array
+    console.log(Object.fromEntries(data)) // Here we just simple display the Array information
+  }
+  return (
+      // * onSubmit will trigger the 'submit' function
+      <form onSubmit={submit}>
+      <div className="App">
+        <span>lala</span>
+        <input name='campo' />
+      </div>
+      <input name='campo2' />
+      <input type='submit' value='Enviar' />
+    </form>
+  );
+}
+
+export default FormUncontrolledComponent;
