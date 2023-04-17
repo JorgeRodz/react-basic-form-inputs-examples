ff36805546adb9bcffc32b286b844c969d38e3a2
Jorge Rodriguez <jarf4321@gmail.com>
2023-04-17 11:49:00 -0600

    Add "Form controlled component" example

---
+++ src/ControlledComponent.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const ControlledComponent = () => {
+  const [value, setValue] = useState('')
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+  console.log(value)
+
+  return (
+    <div>
+      {value.length < 5 ? <span>longitud minima de 5</span> : null }
+      <input type="text" name='normal' value={value} onChange={handleChange}/>
+    </div>
+  )
+}
+
+export default ControlledComponent
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import FormUncontrolledComponent from './FormUncontrolledComponent';
 import ReactFormU from './ReactFormUC';
+import ControlledComponent from './ControlledComponent';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <FormUncontrolledComponent  />
+    <ControlledComponent />
   </React.StrictMode>
 );
 
